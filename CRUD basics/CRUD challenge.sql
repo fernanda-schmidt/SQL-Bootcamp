794546590d9fe576f3bf532d9c3abd6206858bc2
fernanda-schmidt <fefe_sac@icloud.com>
2026-07-28 14:02:35 -0300

    CRUD challenge

---
+++ CRUD basics/CRUD challenge.sql
@@ -0,0 +1,59 @@
+-- create the table
+CREATE TABLE shirts 
+(
+	shirt_id int PRIMARY KEY AUTO_INCREMENT,
+	article varchar(25),
+	color varchar(25),
+	shirt_Size varchar(5),
+	last_worn int
+);
+
+INSERT INTO shirts (article, color, shirt_size, last_worn)  
+VALUES 
+	('t-shirt', 'white', 'S', 10),
+	('t-shirt', 'green', 'S', 200),
+	('polo shirt', 'black', 'M', 10),
+	('tank top', 'blue', 'S', 50),
+	('t-shirt', 'pink', 'S', 0),
+	('polo shirt', 'red', 'M', 5),
+	('tank top', 'white', 'S', 200),
+	('tank top', 'blue', 'M', 15);
+
+INSERT INTO shirts (color, article, shirt_size, last_worn)
+VALUES ('Purple', 'Polo Shirt', 'M', 50);
+
+DESC shirts;
+
+SELECT * FROM shirts;
+
+SELECT article, color FROM shirts;
+
+SELECT article, color, shirt_size, last_worn FROM shirts
+WHERE shirt_size = 'M';
+
+UPDATE shirts SET shirt_size = 'L'
+WHERE article = 'Polo shirt';
+
+SELECT* FROM shirts;
+
+UPDATE shirts SET last_worn = 0
+WHERE LAST_worn = 15;
+
+SELECT* FROM shirts;
+
+UPDATE shirts SET color = 'off white', shirt_SIZE = 'XS'
+WHERE color = 'white';
+
+SELECT* FROM shirts;
+
+DELETE FROM shirts
+WHERE last_worn >= 200;
+
+DELETE FROM shirts 
+WHERE article = 'tank top';
+
+SELECT * FROM shirts;
+
+DELETE FROM shirts;
+
+DROP TABLE shirts;
