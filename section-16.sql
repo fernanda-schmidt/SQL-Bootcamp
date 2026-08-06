b346da72613419b3b92712f2dfd19ac6c816a8e2
fernanda-schmidt <fefe_sac@icloud.com>
2026-08-06 11:14:56 -0300

    first_value

--- section-16.sql
+++ section-16.sql
@@ -110,7 +110,15 @@ FROM employees
 ORDER BY department;
 
 -- FIRST_VALUE
+SELECT 
+    emp_no, 
+    department, 
+    salary,
+    FIRST_VALUE(emp_no) OVER(PARTITION BY department ORDER BY salary DESC) as highest_paid_dept,
+    FIRST_VALUE(emp_no) OVER(ORDER BY salary DESC) as highest_paid_overall
+FROM employees;
 
+-- 
 
 
 
